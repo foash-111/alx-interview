31d7952f884be5ef8a01c646e465a3eef05167a6
foash-111 <mr2221@fayoum.edu.eg>
2024-07-25 15:04:04 +0300

    0x00-pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,35 @@
 #!/usr/bin/python3
 """
 pascal triangle
+
 # every index except the first and last index is the sum of
 the index above and the (index - 1) of the above array
 <array in the index before>
+
+Usage:
+    To use this script, call the `pascal_triangle(n)` function
+    with the desired number of rows.
+
+ Example:
+        >>> pascal_triangle(5)
+        [
+            [1],
+            [1, 1],
+            [1, 2, 1],
+            [1, 3, 3, 1],
+            [1, 4, 6, 4, 1]
+        ]
 """
 
 
 def pascal_triangle(n):
+    """ Args:
+        n (int): The number of rows of Pascal's Triangle to generate.
+
+    Returns:
+        list: A list of lists,
+        where each inner list represents a row in Pascal's Triangle.
+    """
     if n <= 0:
         return []
     else:
