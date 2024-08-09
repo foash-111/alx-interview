3b63ac5abcd5cbeb8c7c575dc02b695a24d1c192
foash-111 <mr2221@fayoum.edu.eg>
2024-08-09 06:20:21 +0300

    min operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,21 +4,34 @@
 
 def minOperations(n):
     """min operation"""
+    if n == 1 or n == 0:
+        return 0
+ 
     count = 0
     if n != 2 and n % 2 != 0:
-        count = 3
-        while (count <= n):
-            if count % n == 0:
+        counter = 3
+        flag = 0
+        while (counter <= n):
+            if counter % n == 0:
+                flag = 1
                 break
-            count += 2
-        return 0
+            counter += 2
+        if flag != 1:
+            return 0
 
     while (n % 3 == 0 and n != 0):
         count += 3
         n = n / 3
+        if n == 1:
+            n = 0
 
-    while (n != 0 and n % 2 == 0):
+    while (n % 2 == 0 and n != 0):
         count += 2
         n = n / 2
+        if n == 1:
+            n = 0
 
+    while (n > 0):
+        n -= 1
+        count += 1
     return count
