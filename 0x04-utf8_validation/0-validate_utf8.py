f51a8dd77fde3b1edbb4466f296f46be75b29ff1
foash-111 <mr2221@fayoum.edu.eg>
2024-08-25 12:36:51 +0300

    valid utf-t

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,39 +13,29 @@ def validUTF8(data):
     the .decode('utf-8') method
     is called on the resulting bytes object.
     """
-    remain_bytes = 0
-    # num_bytes = 0
+    bytes_remaining = 0
 
-    # if it list we will itarate over it
-    # if it string we will itarate over it's characters
-
-    for i in data:
+    for num in data:
         # Convert the integer to a binary string of 8 bits
-        binary_representation = format(i, '08b')
-        if remain_bytes == 0:
-            if binary_representation[0] == '0':
-                pass  # 1 byte character
-            elif binary_representation[:3] == '110':
-                # num_bytes = 2
-                remain_bytes = 1
-            elif binary_representation[:4] == '1110':
-                # num_bytes = 3
-                # the next 2 numbers (bytes) Expected to start with 10xx xxxx
-                # if number[0] == '1110 XXXX' so it's 3 bytes number and,
-                # it's expected the number in [1], [2] to consider as:
-                # continution bytes and start with '10xxx xxxx'
+        bin_rep = format(num, '#010b')[-8:]
 
-                remain_bytes = 2
-            elif binary_representation[:5] == '11110':
-                # num_bytes = 4
-                remain_bytes = 3
+        if bytes_remaining == 0:
+            # Determine the number of bytes in the UTF-8 character
+            if bin_rep[0] == '0':
+                continue  # 1-byte character
+            elif bin_rep[:3] == '110':
+                bytes_remaining = 1  # 2-byte character
+            elif bin_rep[:4] == '1110':
+                bytes_remaining = 2  # 3-byte character
+            elif bin_rep[:5] == '11110':
+                bytes_remaining = 3  # 4-byte character
             else:
-                # Invalid UTF-8 start byte
-                return False
+                return False  # Invalid UTF-8 start byte
         else:
-            #  Continuation byte must start with '10'
-            if binary_representation[:2] != '10':  # continue byte
+            # Continuation byte must start with '10'
+            if bin_rep[:2] != '10':
                 return False
-            remain_bytes -= 1
+            bytes_remaining -= 1
+
     # If we're expecting continuation bytes but didn't get them
-    return remain_bytes == 0
+    return bytes_remaining == 0
