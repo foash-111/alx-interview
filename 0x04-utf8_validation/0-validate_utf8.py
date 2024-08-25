e46ad838ee14e3f47e24ad1712df047e6ddb4469
foash-111 <mr2221@fayoum.edu.eg>
2024-08-25 12:07:34 +0300

    valid utf-t

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,19 +13,38 @@ def validUTF8(data):
     the .decode('utf-8') method
     is called on the resulting bytes object.
     """
-    if isinstance(data, str):
-        try:
-            data.encode('utf-8').decode('utf-8')
-            return True
-        except UnicodeDecodeError:
-            return False
-    else:
-        if isinstance(data, list):
-            for i in data:
-                if isinstance(i, int):
-                    result = i & 127
-                    if result == i:
-                        pass
-                    else:
-                        return False
+    remain_bytes = 0
+    # num_bytes = 0
+
+    # if it list we will itarate over it
+    # if it string we will itarate over it's characters
+
+    for i in data:
+        # Convert the integer to a binary string of 8 bits
+        binary_representation = format(i, '08b')
+        if remain_bytes == 0:
+            if binary_representation[0] == '0':
+                pass  # 1 byte character
+            elif binary_representation[:3] == '110':
+                # num_bytes = 2
+                remain_bytes = 1
+            elif binary_representation[:4] == '1110':
+                # num_bytes = 3
+                # the next 2 numbers (bytes) Expected to start with 10xx xxxx
+                # if number[0] == '1110 XXXX' so it's 3 bytes number and,
+                # it's expected the number in [1], [2] to consider as:
+                # continution bytes and start with '10xxx xxxx'
+
+                remain_bytes = 2
+            elif binary_representation[:5] == '11110':
+                # num_bytes = 4
+                remain_bytes = 3
+            else:
+                # Invalid UTF-8 start byte
+                return False
+        else:
+            #  Continuation byte must start with '10'
+            if binary_representation[:2] != '10':  # continue byte
+                return False
+            remain_bytes -= 1
     return True
