7024eb394b20b23e024ba43627280179f6deccdf
foash-111 <mr2221@fayoum.edu.eg>
2024-09-12 23:37:16 +0300

    rotate matrix in place

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,20 +4,19 @@
 Given an n x n 2D matrix, rotate it 90 degrees clockwise.
 """
 
-def rotate_2d_matrix(matrix=[]):
-	"""
-	Do not return anything. The matrix must be edited in-place.
-	You can assume the matrix will have 2 dimensions and will not be empty.
-	"""
-	outer_list = []
-	for i in range(len(matrix)):
-		inner_list = []
-		for j in matrix.__reversed__():
-			inner_list.append(j[i])
-		outer_list.append(inner_list)
-	
-	for k in range(len(matrix)):
-		for l in range(len(matrix)):
-			matrix[k][l] = outer_list[k][l]
 
-	
+def rotate_2d_matrix(matrix=[]):
+    """
+    Do not return anything. The matrix must be edited in-place.
+    You can assume the matrix will have 2 dimensions and will not be empty.
+    """
+    outer_list = []
+    for i in range(len(matrix)):
+        inner_list = []
+        for j in matrix.__reversed__():
+            inner_list.append(j[i])
+        outer_list.append(inner_list)
+
+    for x in range(len(matrix)):
+        for y in range(len(matrix)):
+            matrix[x][y] = outer_list[x][y]
