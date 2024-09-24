be49b879872bf37f7ddcd078917ae0206bd97eb8
foash-111 <mr2221@fayoum.edu.eg>
2024-09-24 16:03:33 +0300

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""find island perimeter"""
+"""Find island perimeter"""
 
 
 def island_perimeter(grid):
-    """find island perimeter"""
+    """Find island perimeter"""
     perimeter = 0
-    flag = 0
+    rows = len(grid)
+    cols = len(grid[0])
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if (grid[i][j]) == 1 and flag == 0:
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:  # If it's a land cell
+                # Start by adding 4 for this cell's perimeter
                 perimeter += 4
-                flag = 1
-                # set flag to 1 to not enter this condition again
-            try:
-                if grid[i][j] == 1:
-                    if (grid[i][j - 1] == 1) != (grid[i - 1][j] == 1):
-                        perimeter += 2
-            except IndexError:
-                pass
+
+                # Check the cell above (i-1, j)
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2  # Subtract 2 for the shared side
+
+                # Check the cell to the left (i, j-1)
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2  # Subtract 2 for the shared side
 
     return perimeter
